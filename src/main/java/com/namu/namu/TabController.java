cd65815cc9a01c79c213929a32edb54988aa7061
Mery <kgs246531@gmail.com>
2025-06-08 20:36:50 +0900

    namu 2

--- src/main/java/com/namu/namu/TabController.java
+++ src/main/java/com/namu/namu/TabController.java
@@ -2,6 +2,8 @@ package com.namu.namu;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -10,5 +12,11 @@ public class TabController {
     public String indextab() {
         return "test.html";
     }
+
+    @GetMapping("/booktype")
+    public String booktype() {
+        return "api/typetab.html";
+    }
+    
     
 }
